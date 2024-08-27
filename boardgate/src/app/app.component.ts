147d6b97d1eb3b3df9c60dd5ab7cc9edea595750
Tony Reid <Tony.Reid@imglobal.com>
2024-08-27 15:29:09 -0400

    Lesson 2

--- boardgate/src/app/app.component.ts
+++ boardgate/src/app/app.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatMomentDateModule } from '@angular/material-moment-adapter';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [
+    CommonModule, 
+    RouterOutlet, 
+    MatSlideToggleModule,
+    MatInputModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatMomentDateModule,
+  ],
+  templateUrl : './app.component.html',
+  styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'boardgate';
-}
+
+export class AppComponent{
+
+  title= 'BOARD GATE'
+  
+ }
