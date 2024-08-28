8ca674ae661eb4c450a1e9438599dcb75e4446df
Tony Reid <Tony.Reid@imglobal.com>
2024-08-28 11:17:54 -0400

    Form and Validation Lesson

--- boardgate/src/app/app.component.ts
+++ boardgate/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import {MatSlideToggleModule} from '@angular/material/slide-toggle';
@@ -6,6 +6,7 @@ import {MatInputModule} from '@angular/material/input';
 import {MatSelectModule} from '@angular/material/select';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import { MatMomentDateModule } from '@angular/material-moment-adapter';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -17,13 +18,19 @@ import { MatMomentDateModule } from '@angular/material-moment-adapter';
     MatSelectModule,
     MatDatepickerModule,
     MatMomentDateModule,
+    ReactiveFormsModule,
   ],
   templateUrl : './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.css'
 })
 
 export class AppComponent{
+    fb = inject(FormBuilder)
 
-  title= 'BOARD GATE'
-  
+    form = this.fb.group({
+      name: ['',[Validators.required, Validators.minLength(3)]],
+      favoriteFood: [''],
+      dateOfBirth: [''],
+    });
+    title= 'BOARD GATE'
  }
